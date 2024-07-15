f5e443ce849f376e6ad011d66bb7dd2e395f116b
benson toh <benson_7667@hotmail.com>
2024-07-15 15:51:53 +0800

    chore: details page

---
+++ src/app/details/page.js
@@ -0,0 +1,83 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export default function Details() {
+  const router = useRouter();
+
+  return (
+    <main>
+      <div className="min-h-screen flex flex-col bg-white flex-grow">
+        <header className="flex">
+          <button
+            className="w-[55px] h-[55px] flex justify-center items-center text-gray-500"
+            onClick={() => router.back()}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 19.5L8.25 12l7.5-7.5"
+              />
+            </svg>
+          </button>
+          <div className="flex-grow text-center self-center text-gray-500">
+            View GIRO arrangement
+          </div>
+          <div className="w-[55px] h-[55px] flex justify-center items-center text-gray-500"></div>
+        </header>
+
+        <div className="p-6">
+          <div className="text-gray-500">Billing organisation</div>
+          <div className="text-2xl font-semibold">Merchant name</div>
+
+          <div className="mt-2">
+            <span className="inline-block bg-gray-200 text-gray-800 text-xs px-2 py-1 rounded-full">
+              In progress
+            </span>
+          </div>
+
+          <div className="mb-1 text-gray-500 mt-4">Created on</div>
+          <div className="font-semibold text-gray-600">1 Mar 2024</div>
+        </div>
+
+        <div className="border-t border-gray-300 my-4"></div>
+
+        <div className="p-6">
+          <div className="mb-6">
+            <div className="mb-1 text-gray-500">Billing reference number</div>
+            <div className="font-semibold text-gray-600">123-456-789</div>
+          </div>
+          <div className="mb-6">
+            <div className="mb-1 text-gray-500">Payment limit</div>
+            <div className="font-semibold text-gray-600">100.00 SGD</div>
+          </div>
+          <div className="mb-6">
+            <div className="mb-1 text-gray-500">Payment type</div>
+            <div className="font-semibold text-gray-600">Pay full amount</div>
+          </div>
+          <div className="mb-6">
+            <div className="mb-1 text-gray-500">Name</div>
+            <div className="font-semibold text-gray-600">Ashley Tan</div>
+          </div>
+          <div className="mb-6">
+            <div className="mb-1 text-gray-500">Contact number</div>
+            <div className="font-semibold text-gray-600">1234 1234</div>
+          </div>
+          <div>
+            <div className="mb-1 text-gray-500">From</div>
+            <div className="font-semibold text-gray-600">360 Account</div>
+            <div className="mb-1 text-gray-500">123-45678-999</div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <div className="min-h-screen flex flex-col bg-gradient-to-b from-[#F5E1DA4D] to-[#D8CAE14D]">
-        {/* <header class="flex items-center space-x-2 mb-4">
-          <button class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 w-10">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="h-6 w-6"
-            >
-              <path d="m15 18-6-6 6-6"></path>
-            </svg>
-          </button>
-        </header> */}
-
         <div className="p-6 mt-4">
           <h1 className="text-2xl font-semibold mb-4">
-            View / Terminate GIRO arrangements 2nd deployment
+            View / Terminate GIRO arrangements
           </h1>
           <p className="text-sm text-gray-600 mb-6">
             Unsuccessful/Terminated GIRO arrangements will only be shown on
@@ -61,51 +44,77 @@ export default function Home() {
             </button>
           </div>
 
-          <div className="bg-gray-50 p-4 rounded-lg mb-4">
-            <h3 className="font-semibold">OCBC Credit Cards</h3>
-            <p className="text-gray-700">1234-5678-2244-6688</p>
-            <p className="text-gray-500">Pay full amount</p>
-          </div>
-          <div className="bg-gray-50 p-4 rounded-lg mb-4">
-            <h3 className="font-semibold">IRAS - GST</h3>
-            <p className="text-gray-700">1234567890</p>
-            <p className="text-gray-500">Pay full amount annually</p>
-          </div>
-          <div className="bg-gray-50 p-4 rounded-lg mb-4">
-            <h3 className="font-semibold">Ang Mo Kio Town Council</h3>
-            <p className="text-gray-700">1234567890</p>
-            <p className="text-gray-500">Pay minimum amount</p>
-          </div>
-          <div className="bg-gray-50 p-4 rounded-lg mb-4">
-            <h3 className="font-semibold">OCBC Credit Cards</h3>
-            <p className="text-gray-700">1234-5678-2244-6688</p>
-            <p className="text-gray-500">Pay full amount</p>
-          </div>
-          <div className="bg-gray-50 p-4 rounded-lg mb-4">
-            <h3 className="font-semibold">IRAS - GST</h3>
-            <p className="text-gray-700">1234567890</p>
-            <p className="text-gray-500">Pay full amount annually</p>
-          </div>
-          <div className="bg-gray-50 p-4 rounded-lg mb-4">
-            <h3 className="font-semibold">Ang Mo Kio Town Council</h3>
-            <p className="text-gray-700">1234567890</p>
-            <p className="text-gray-500">Pay minimum amount</p>
-          </div>
-          <div className="bg-gray-50 p-4 rounded-lg mb-4">
-            <h3 className="font-semibold">OCBC Credit Cards</h3>
-            <p className="text-gray-700">1234-5678-2244-6688</p>
-            <p className="text-gray-500">Pay full amount</p>
-          </div>
-          <div className="bg-gray-50 p-4 rounded-lg mb-4">
-            <h3 className="font-semibold">IRAS - GST</h3>
-            <p className="text-gray-700">1234567890</p>
-            <p className="text-gray-500">Pay full amount annually</p>
-          </div>
-          <div className="bg-gray-50 p-4 rounded-lg mb-4">
-            <h3 className="font-semibold">Ang Mo Kio Town Council</h3>
-            <p className="text-gray-700">1234567890</p>
-            <p className="text-gray-500">Pay minimum amount</p>
-          </div>
+          <Link href="/details">
+            <div className="bg-gray-50 p-4 rounded-lg mb-4">
+              <h3 className="font-semibold">OCBC Credit Cards</h3>
+              <p className="text-gray-700">1234-5678-2244-6688</p>
+              <p className="text-gray-500">Pay full amount</p>
+            </div>
+          </Link>
+
+          <Link href="/details">
+            <div className="bg-gray-50 p-4 rounded-lg mb-4">
+              <h3 className="font-semibold">IRAS - GST</h3>
+              <p className="text-gray-700">1234567890</p>
+              <p className="text-gray-500">Pay full amount annually</p>
+            </div>
+          </Link>
+
+          <Link href="/details">
+            <div className="bg-gray-50 p-4 rounded-lg mb-4">
+              <h3 className="font-semibold">Ang Mo Kio Town Council</h3>
+              <p className="text-gray-700">1234567890</p>
+              <p className="text-gray-500">Pay minimum amount</p>
+            </div>
+          </Link>
+
+          <Link href="/details">
+            <div className="bg-gray-50 p-4 rounded-lg mb-4">
+              <h3 className="font-semibold">OCBC Credit Cards</h3>
+              <p className="text-gray-700">1234-5678-2244-6688</p>
+              <p className="text-gray-500">Pay full amount</p>
+            </div>
+          </Link>
+
+          <Link href="/details">
+            <div className="bg-gray-50 p-4 rounded-lg mb-4">
+              <h3 className="font-semibold">IRAS - GST</h3>
+              <p className="text-gray-700">1234567890</p>
+              <p className="text-gray-500">Pay full amount annually</p>
+            </div>
+          </Link>
+
+          <Link href="/details">
+            <div className="bg-gray-50 p-4 rounded-lg mb-4">
+              <h3 className="font-semibold">Ang Mo Kio Town Council</h3>
+              <p className="text-gray-700">1234567890</p>
+              <p className="text-gray-500">Pay minimum amount</p>
+            </div>
+          </Link>
+
+          <Link href="/details">
+            <div className="bg-gray-50 p-4 rounded-lg mb-4">
+              <h3 className="font-semibold">OCBC Credit Cards</h3>
+              <p className="text-gray-700">1234-5678-2244-6688</p>
+              <p className="text-gray-500">Pay full amount</p>
+            </div>
+          </Link>
+
+          <Link href="/details">
+            <div className="bg-gray-50 p-4 rounded-lg mb-4">
+              <h3 className="font-semibold">IRAS - GST</h3>
+              <p className="text-gray-700">1234567890</p>
+              <p className="text-gray-500">Pay full amount annually</p>
+            </div>
+          </Link>
+
+          <Link href="/details">
+            <div className="bg-gray-50 p-4 rounded-lg mb-4">
+              <h3 className="font-semibold">Ang Mo Kio Town Council</h3>
+              <p className="text-gray-700">1234567890</p>
+              <p className="text-gray-500">Pay minimum amount</p>
+            </div>
+          </Link>
         </div>
       </div>
 
