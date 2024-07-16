beeea4822211f11e0116f291f6790f4c20881202
benson toh <benson_7667@hotmail.com>
2024-07-16 17:26:53 +0800

    chore: add back easy speech and translation select

--- src/app/template.js
+++ src/app/template.js
@@ -1,5 +1,18 @@
 "use client";
 
 export default function Template({ children }) {
-  return <div>{children}</div>;
+  return (
+    <div className="absolute">
+      {children}
+      <easy-speech right="130px" />
+      <translation-select
+        langs="en-GB,de,fr,zh"
+        selectui="minimal"
+        bottom="50px"
+        right="15px"
+        height="55px"
+        borderthickness="1px"
+      />
+    </div>
+  );
 }
