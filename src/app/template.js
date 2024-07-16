8ddd9547e7eb08755a551b5159322aff68849137
benson toh <benson_7667@hotmail.com>
2024-07-16 15:06:54 +0800

    fix absolute

--- src/app/template.js
+++ src/app/template.js
@@ -1,9 +1,5 @@
 "use client";
 
-import IconSpeech from "@/components/icons/speech";
-import IconTranslate from "@/components/icons/translate";
-import { useEffect } from "react";
-
 export default function Template({ children }) {
-  return <div className="absolute">{children}</div>;
+  return <div>{children}</div>;
 }
